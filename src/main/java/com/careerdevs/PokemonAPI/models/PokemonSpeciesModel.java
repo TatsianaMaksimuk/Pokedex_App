64aa6245a6e31cfb0aea609d2c9c069a9b7a23d6
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-12-24 15:57:38 -0800

    PokemonSpecies model added

--- src/main/java/com/careerdevs/PokemonAPI/models/PokemonSpeciesModel.java
+++ src/main/java/com/careerdevs/PokemonAPI/models/PokemonSpeciesModel.java
@@ -1,4 +1,18 @@
 package com.careerdevs.PokemonAPI.models;
 
 public class PokemonSpeciesModel {
+    private int id;
+    private String name;
+    private int order;
+    private int capture_rate;
+    private boolean is_legendary;
+    private boolean is_mythical;
+    private Egg_groups[] egg_groups;
+        private static class Egg_groups{
+            private int id;
+            private String url;
+        }
+
+    private String color;
+
 }
